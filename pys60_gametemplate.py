619807ba91199818d985ec43bd3ee1e1fff81ff8
howdyworld <howdy.earth@gmail.com>
2014-09-06 11:57:30 +0300

    variable for game dir name

--- pys60_gametemplate.py
+++ pys60_gametemplate.py
@@ -5,10 +5,11 @@ import os
 import sys
 import e32
 
+GAME_DIRNAME = u"gametemplate"
 # looking for install dir   
 DEFDIR = u"e:\\python"
 for drive in e32.drive_list():
-    appd = os.path.join(drive, u"\\data\\python\\gametemplate\\")
+    appd = os.path.join(drive, u"\\data\\python\\%s\\" % GAME_DIRNAME)
     if os.path.exists(os.path.join(appd, u"game.py")):
         DEFDIR = appd
         break
